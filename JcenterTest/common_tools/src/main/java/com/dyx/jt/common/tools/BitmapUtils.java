97342ea72b8f3985822e9c4efc81ff2b65d5eae5
DoubleDa <yongxinda89@gmail.com>
2017-12-05 18:17:11 +0800

    upload JcenterTest project

---
+++ JcenterTest/common_tools/src/main/java/com/dyx/jt/common/tools/BitmapUtils.java
@@ -0,0 +1,11 @@
+package com.dyx.jt.common.tools;
+
+/**
+ * Author：dayongxin
+ * Function：
+ */
+public class BitmapUtils {
+    public static String getInfo() {
+        return "This is a bitmap utils!";
+    }
+}
